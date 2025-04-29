4c5a08c953eaa5404f6bbdf7f58bb1439c74d4d4
kenyen-market <kenyen60108@gmail.com>
2025-04-29 10:05:49 +0800

    Update tsmc_watcher.py

--- tsmc_watcher.py
+++ tsmc_watcher.py
@@ -48,8 +48,8 @@ def get_price_data():
             print(">>> 資料抓取失敗或缺少欄位")
             return None
 
-        current_price = df["Close"].iloc[-1].item()
-        ma20 = df["Close"].rolling(window=20).mean().iloc[-1]
+       current_price = df["Close"].iloc[-1].item()
+ma20 = df["Close"].rolling(window=20).mean().iloc[-1].item()
         if pd.isna(ma20):
             print(">>> MA20 資料不足")
             return None
